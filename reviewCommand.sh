278f9693d32fc00398fbcc3567dacd352e4f1ac4
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-07-01 13:32:48 +0200

    Feature: complete_review is ready for execution

--- reviewCommand.sh
+++ reviewCommand.sh
@@ -16,8 +16,10 @@ while true; do
         output=$($command)
     else
         uuid=$(python3 ./uuidFinder.py)
-        add_comment="wtc-lms add_comment $uuid \"Great work\""
-        echo $add_comment
+        add_comment="wtc-lms add_comment $uuid \"Beautiful\""
+        echo $($add_comment)
+        complete_review="wtc-lms complete_review $uuid"
+        echo $($complete_review)
         break
     fi
 done
