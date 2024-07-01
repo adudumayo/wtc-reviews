5a4b647a1873338ae7038bdf3426d8223a0fab82
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-07-01 13:54:30 +0200

    Refactor: Cleaner code and output string formatting

--- reviewCommand.sh
+++ reviewCommand.sh
@@ -5,21 +5,26 @@ command="wtc-lms reviews"
 notDesiredOutput="listing reviews failed because Request(reqwest::Error { kind: Decode, source: TimedOut })"
 
 while true; do
-    # Get the output
     output=$($command)
-
     echo "$output">output.txt
 
-    # Check if the output contains the notDesiredOutput string 
     if [[ $output == *"$notDesiredOutput"* ]]; then
         echo "Running the command again..."
         output=$($command)
     else
         uuid=$(python3 ./uuidFinder.py)
+        echo "Now reviewing : $uuid" && echo
+
+        echo "Looking for the best comment for : $uuid ..." && sleep 2
         add_comment="wtc-lms add_comment $uuid \"Beautiful\""
+        echo "Now running $add_comment" && echo && sleep 2
         echo $($add_comment)
+
+        echo "Completing review for : $uuid"
         complete_review="wtc-lms complete_review $uuid"
+        echo "Now running $complete_review" && echo && sleep 2
         echo $($complete_review)
+
         break
     fi
 done
